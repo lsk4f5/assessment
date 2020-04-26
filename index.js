3204e2d5882711c5bdbd730732d78bdc1e40cf0a
lsk4f5 <miseri.osk12@gmail.com>
2020-04-27 01:58:51 +0900

    結果を消す

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const assessmentButton = document.getElementById('assessment');
 const resultDivided = document.getElementById('result-area');
 const tweetDivided = document.getElementById('tweet-area');
 
+/**
+ * 指定した要素の子どもを全て削除する
+ * @param {HTMLElement} element HTMLの要素
+ */
+function removeAllChildren(element) {
+    while (element.firstChild) { // 子どもの要素があるかぎり削除
+      element.removeChild(element.firstChild);
+    }
+  }
+
 assessmentButton.onclick = () => {
   const userName = userNameInput.value;
   if (userName.length === 0) { // 名前が空の時は処理を終了する
